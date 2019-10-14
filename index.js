f9b0c600d5093f66d81ebffc6e271bf3b1d8a404
bouiboui <dev@cod3.net>
2019-10-14 23:10:09 +0200

    Added support for named parameters

--- index.js
+++ index.js
@@ -5,17 +5,28 @@ const ffmpeg_static = require('ffmpeg-static');
 const mime = require('mime');
 const fs = require('fs');
 
-module.exports = (filePathIn, filePathOut) => new Promise((resolve, reject) => {
+async function linear16(filePathIn, filePathOut) {
+
+    if (('object' === typeof filePathIn) && !filePathOut) {
+        const {inPath, outPath} = filePathIn;
+        filePathIn = inPath;
+        filePathOut = outPath;
+    }
+
     if (!filePathIn || !filePathOut) {
         throw new Error('You must specify a path for both input and output files.');
     }
     if (!fs.existsSync(filePathIn)) {
         throw new Error('Input file must exist.');
     }
-    if (mime.getType(filePathIn).indexOf('audio') > -1) {
+    if (mime.getType(filePathIn).indexOf('audio') <= -1) {
+        throw new Error('File must have audio mime.');
+    }
+
+    return new Promise((resolve, reject) => {
         try {
             ffmpeg()
-                .setFfmpegPath(ffmpeg_static.path) 
+                .setFfmpegPath(ffmpeg_static.path)
                 .input(filePathIn)
                 .outputOptions([
                     '-f s16le',
@@ -31,7 +42,8 @@ module.exports = (filePathIn, filePathOut) => new Promise((resolve, reject) => {
         } catch (e) {
             reject(e);
         }
-    } else {
-        throw new Error('File must have audio mime.');
-    }
-});
+    });
+
+}
+
+module.exports = linear16;
--- test/linear16test.js
+++ test/linear16test.js
@@ -7,34 +7,26 @@ const it = require('mocha').it;
 const fs = require('fs');
 
 describe('linear16', () => {
-    it('should fail if parameters are missing', () => {
-        linear16().then(() => {
-            throw new Error('should have rejected input');
-        }).catch(err => {
-        });
-        linear16('./test').then(() => {
-            throw new Error('should have rejected input');
-        }).catch(err => {
-        });
-        linear16(null, './test').then(() => {
-            throw new Error('should have rejected input');
-        }).catch(err => {
-        });
+    it('should fail if parameters are missing', async () => {
+        return assert.rejects(linear16.bind(null), Error);
     });
-    it('should fail for non-existing input file', () => {
-        linear16('./thisfiledoesnotexist', './out')
-            .then(() => {
-                throw new Error('Should have thrown an error');
-            })
-            .catch(() => {
-            });
+    it('should fail if input file parameter is missing', async () => {
+        return assert.rejects(linear16.bind(null, './test'), Error);
     });
-    it('should work normally', () =>
-        linear16(
-            './test/test.m4a',
-            './test/test.wav'
-        ).then(outFile => {
-            assert.deepEqual(true, fs.existsSync(outFile));
-            fs.unlinkSync(outFile)
-        }))
+    it('should fail if output file parameter is missing', async () => {
+        return assert.rejects(linear16.bind(null, null, './test'), Error);
+    });
+    it('should fail for non-existing input file', async () => {
+        return assert.rejects(linear16.bind(null, './thisfiledoesnotexist', './out'), Error);
+    });
+    it('should work normally', async () => {
+        const outFile = await linear16('./test/test.m4a', './test/test.wav');
+        assert.deepEqual(true, fs.existsSync(outFile));
+        fs.unlinkSync(outFile);
+    });
+    it('should accept object-like parameters', async () => {
+        const outFile = await linear16({inPath: './test/test.m4a', outPath: './test/test.wav'});
+        assert.deepEqual(true, fs.existsSync(outFile));
+        fs.unlinkSync(outFile);
+    });
 });
